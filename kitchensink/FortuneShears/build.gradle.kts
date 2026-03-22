12251184066a0a02b238cde844677ff885083d74
Jikoo <jikoo.games@gmail.com>
2026-03-22 10:18:49 -0400

    Relocate and minimize enchanting

--- kitchensink/FortuneShears/build.gradle.kts
+++ kitchensink/FortuneShears/build.gradle.kts
@@ -5,3 +5,8 @@ plugins {
 dependencies {
   implementation(libs.planarenchanting)
 }
+
+tasks.shadowJar {
+  relocate("com.github.jikoo.planarenchanting", "com.github.jikoo.planarplugins.fortuneshears.lib.enchanting")
+  minimize()
+}
