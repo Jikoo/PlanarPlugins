4401d0d9991dcc29c59654dc832c5ebd632de783
Jikoo <jikoo.games@gmail.com>
2026-03-24 14:01:44 -0400

    Fix potential nullity issue
    I even had nullity analysis enabled on my craptop, but I guess it was too strained to notice :(

--- kitchensink/ColorSignText/src/main/java/com/github/jikoo/planarplugins/ColorSignText.java
+++ kitchensink/ColorSignText/src/main/java/com/github/jikoo/planarplugins/ColorSignText.java
@@ -1,31 +1,38 @@
 package com.github.jikoo.planarplugins;
 
+import com.google.errorprone.annotations.Keep;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jspecify.annotations.NullMarked;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.format.Style;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.jspecify.annotations.Nullable;
 
 @NullMarked
 public class ColorSignText extends JavaPlugin implements Listener {
-  
+
   @Override
   public void onEnable() {
     getServer().getPluginManager().registerEvents(this, this);
   }
 
   @EventHandler
+  @Keep
   private void onSignChange(SignChangeEvent event) {
-    for (int index = event.lines().size() - 1; index <= 0; --index) {
+    for (int index = event.lines().size() - 1; index >= 0; --index) {
       event.line(index, translateCodes(event.line(index)));
     }
   }
 
-  private Component translateCodes(Component component) {
+  private @Nullable Component translateCodes(@Nullable Component component) {
+    if (component == null) {
+      return null;
+    }
+
     if (!Component.IS_NOT_EMPTY.test(component)
         || !(component instanceof TextComponent text)) {
       return component;
